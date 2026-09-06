4c41f10d0c03d2b24029401ad3b7936852aae9ba
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-06 19:15:29 -0400

    style: format manifest module

--- apps/api/src/manifiestos/manifiestos.module.ts
+++ apps/api/src/manifiestos/manifiestos.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { GeocodificacionModule } from '../geocodificacion/geocodificacion.module.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 import { ManifiestosController } from './manifiestos.controller.js';
 import { ManifiestosDiagnosticoService } from './manifiestos.diagnostico.service.js';
 import { ManifiestosImportacionProgressService } from './manifiestos.importacion.progress.service.js';
 import { ManifiestosService } from './manifiestos.service.js';
 import { ManifiestoParser } from './parsers/manifiesto.parser.js';
-import { GeocodificacionModule } from '../geocodificacion/geocodificacion.module.js';
-import { PrismaModule } from '../prisma/prisma.module.js';
-@Module({imports:[PrismaModule,GeocodificacionModule],controllers:[ManifiestosController],providers:[ManifiestosService,ManifiestosDiagnosticoService,ManifiestosImportacionProgressService,ManifiestoParser],exports:[ManifiestosService,ManifiestoParser,ManifiestosImportacionProgressService]})
+
+@Module({
+  imports: [PrismaModule, GeocodificacionModule],
+  controllers: [ManifiestosController],
+  providers: [
+    ManifiestosService,
+    ManifiestosDiagnosticoService,
+    ManifiestosImportacionProgressService,
+    ManifiestoParser,
+  ],
+  exports: [
+    ManifiestosService,
+    ManifiestoParser,
+    ManifiestosImportacionProgressService,
+  ],
+})
 export class ManifiestosModule {}
