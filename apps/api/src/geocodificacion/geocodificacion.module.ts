05139fcaf0df4939d166b24807141e47992b45f8
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-07 10:59:18 -0400

    feat(api): activate territorial-aware geocoding

--- apps/api/src/geocodificacion/geocodificacion.module.ts
+++ apps/api/src/geocodificacion/geocodificacion.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { CubaAwareGeocodificacionService } from './cuba-aware-geocodificacion.service.js';
 import { CubaTerritorialService } from './cuba-territorial.service.js';
 import { GeocodificacionService } from './geocodificacion.service.js';
 
 @Module({
-  providers: [CubaTerritorialService, GeocodificacionService],
+  providers: [
+    CubaTerritorialService,
+    CubaAwareGeocodificacionService,
+    {
+      provide: GeocodificacionService,
+      useExisting: CubaAwareGeocodificacionService,
+    },
+  ],
   exports: [GeocodificacionService, CubaTerritorialService],
 })
 export class GeocodificacionModule {}
