3a5700c392dae2c0ad3feb0e36859d0a11e106ed
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-07 11:42:27 -0400

    fix(api): expose PrismaService to geocodificacion module tests

--- apps/api/src/geocodificacion/geocodificacion.module.ts
+++ apps/api/src/geocodificacion/geocodificacion.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { CubaAwareGeocodificacionService } from './cuba-aware-geocodificacion.service.js';
 import { CubaTerritorialService } from './cuba-territorial.service.js';
 import { GeocodificacionService } from './geocodificacion.service.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
+  imports: [PrismaModule],
   providers: [
     CubaTerritorialService,
     CubaAwareGeocodificacionService,
