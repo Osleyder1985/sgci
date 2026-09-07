0a05d140affc17dc7e6e319655e98313fc68b432
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-07 15:02:50 -0400

    feat(import): register durable manifest worker

--- apps/api/src/manifiestos/manifiestos.module.ts
+++ apps/api/src/manifiestos/manifiestos.module.ts
@@ -5,6 +5,8 @@ import { ManifiestosController } from './manifiestos.controller.js';
 import { ManifiestosDiagnosticoService } from './manifiestos.diagnostico.service.js';
 import { ManifiestosImportacionProgressService } from './manifiestos.importacion.progress.service.js';
 import { ManifiestosImportacionEscalableService } from './manifiestos.importacion.escalable.service.js';
+import { ManifiestosImportacionSourceService } from './manifiestos.importacion.source.service.js';
+import { ManifiestosImportacionWorker } from './manifiestos.importacion.worker.js';
 import { ManifiestosService } from './manifiestos.service.js';
 import { ManifiestoParser } from './parsers/manifiesto.parser.js';
 
@@ -16,6 +18,8 @@ import { ManifiestoParser } from './parsers/manifiesto.parser.js';
     ManifiestosDiagnosticoService,
     ManifiestosImportacionProgressService,
     ManifiestosImportacionEscalableService,
+    ManifiestosImportacionSourceService,
+    ManifiestosImportacionWorker,
     ManifiestoParser,
   ],
   exports: [
