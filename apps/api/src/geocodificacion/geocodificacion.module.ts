9c56e4c34e4ca6cb260f3c661d07f7723d80e0a8
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-07 10:59:01 -0400

    feat(api): register Cuban territorial resolver

--- apps/api/src/geocodificacion/geocodificacion.module.ts
+++ apps/api/src/geocodificacion/geocodificacion.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { CubaTerritorialService } from './cuba-territorial.service.js';
 import { GeocodificacionService } from './geocodificacion.service.js';
 
 @Module({
-  providers: [GeocodificacionService],
-  exports: [GeocodificacionService],
+  providers: [CubaTerritorialService, GeocodificacionService],
+  exports: [GeocodificacionService, CubaTerritorialService],
 })
 export class GeocodificacionModule {}
